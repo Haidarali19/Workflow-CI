422bc8ebc60c7467ca908b71b44809d19ae3ca4b
Haidarali19 <haidaralthaaf19@gmail.com>
2026-05-14 13:28:21 +0700

    menjalankan ulang project dengan mlflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -19,24 +19,13 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Mengaktifkan Autolog
 mlflow.sklearn.autolog()
 
-active_run = mlflow.active_run()
-if active_run:
-    print(f"Menggunakan run aktif: {active_run.info.run_id}")
-    mlflow.log_param("model", "LinearRegression") 
+with mlflow.start_run():
+    print(f"Berhasil menyambung ke Run ID: {mlflow.active_run().info.run_id}")
+    mlflow.log_param("model", "LinearRegression")
     model = LinearRegression()
     model.fit(X_train, y_train)
-    
-    # Prediksi
+
+    # Menghitung Prediksi
     y_pred = model.predict(X_test)
     mae = mean_absolute_error(y_test, y_pred)
     print(f"Baseline Model Trained. MAE: {mae}")
-else:
-    with mlflow.start_run(run_name="HousePrice_Baseline_Linear"):
-        mlflow.log_param("model", "LinearRegression")
-        model = LinearRegression()
-        model.fit(X_train, y_train)
-    
-        # Prediksi
-        y_pred = model.predict(X_test)
-        mae = mean_absolute_error(y_test, y_pred)
-        print(f"Baseline Model Trained. MAE: {mae}")
