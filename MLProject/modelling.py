7cd10243c700e558c1e19d70a62a671effeb4f5a
Haidarali19 <haidaralthaaf19@gmail.com>
2026-05-14 13:35:41 +0700

    menjalankan ulang project dengan mlflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -9,6 +9,9 @@ from sklearn.metrics import mean_absolute_error
 # Inisialisasi DagsHub
 dagshub.init(repo_owner='Haidarali19', repo_name='Eksperimen_SML_M.-Haidar', mlflow=True)
 
+# Mengaktifkan Autolog
+mlflow.sklearn.autolog()
+
 # Load Data
 df = pd.read_csv('house_prices_preprocessing.csv')
 X = df.drop('price', axis=1)
@@ -16,16 +19,12 @@ y = df['price']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Mengaktifkan Autolog
-mlflow.sklearn.autolog()
+model = LinearRegression()
+model.fit(X_train, y_train)
 
-with mlflow.start_run():
-    print(f"Berhasil menyambung ke Run ID: {mlflow.active_run().info.run_id}")
-    mlflow.log_param("model", "LinearRegression")
-    model = LinearRegression()
-    model.fit(X_train, y_train)
+# Menghitung Prediksi
+y_pred = model.predict(X_test)
+mae = mean_absolute_error(y_test, y_pred)
+mlflow.log_metric("manual_mae", mae)
 
-    # Menghitung Prediksi
-    y_pred = model.predict(X_test)
-    mae = mean_absolute_error(y_test, y_pred)
-    print(f"Baseline Model Trained. MAE: {mae}")
+print(f"Baseline Model Trained. MAE: {mae}")
